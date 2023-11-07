71d20a1f2e031e8c3b8347924c3dd869da0a34b3
waigoma <waichi0302@gmail.com>
2023-11-08 01:20:54 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "com.waigoma"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation("net.dv8tion:JDA:5.0.0-beta.17")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
