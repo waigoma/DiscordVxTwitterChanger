06c58e42a3697782cb9196bb61689b4d69d549f8
waigoma <48357625+waigoma@users.noreply.github.com>
2023-11-10 12:00:26 +0900

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("net.dv8tion:JDA:5.0.0-beta.17")
     implementation("commons-io:commons-io:2.15.0")
+    implementation("org.yaml:snakeyaml:2.2")
 }
 
 tasks.test {
